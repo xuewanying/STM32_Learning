c98e66c06318025403a0a79679ae59a1997d333b
xuewanying <2328692085@qq.com>
2026-01-27 13:28:46 +0800

    Day010:让有源蜂鸣器响起来 //程序简单，使用新的开发工具，嘿嘿你好新世界

---
+++ 有源蜂鸣器/Buzzer/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Buzzer' 
+ * Target:  'Buzzer' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
